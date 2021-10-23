801276e9bc14f5def3f413f0a2d5f71e7211da88
Tyyagoo <tyyagoosantos@gmail.com>
2021-10-23 13:55:55 -0300

    exe04

--- 00-prework/desafios-pre-work-b-academy/dia1/src/main.js
+++ 00-prework/desafios-pre-work-b-academy/dia1/src/main.js
@@ -4,3 +4,18 @@ document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+document.querySelector('[data-js="anchor"]').addEventListener(
+  "click",
+  (event) => {
+    const app = document.querySelector('[data-js="app"]');
+    if (app.classList.contains("hidden")) {
+      app.classList.remove("hidden");
+      event.target.innerHTML = "Esconder";
+    } else {
+      app.classList.add("hidden");
+      event.target.innerHTML = "Mostrar";
+    }
+  },
+  false
+);
